e7268d9e9512479225edf70ed30c338b3e4ca015
tam-ares <tamara.franco.betancur.25@gmail.com>
2026-06-13 09:58:47 -0400

    icon

---
+++ components/atoms/ChevronIcon.tsx
@@ -0,0 +1,20 @@
+import "./ChevronIcon.css";
+
+function ChevronIcon() {
+  return (
+    <svg
+      className="chevron-icon"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <polyline points="9 18 15 12 9 6" />
+    </svg>
+  );
+}
+
+export default ChevronIcon;
